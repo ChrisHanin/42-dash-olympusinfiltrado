a94263e6823adecc3995950e72a32b28cca0871f
pvilchez <pvilchez@student.42malaga.com>
2026-04-22 19:08:30 +0200

    add header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 function App() {
   return (
-    <main className="app">
-      <section className="card">
-        <h1>QTech Dash</h1>
-        <p>React is now configured with Vite.</p>
-      </section>
-    </main>
+    <>
+      <header className="header">
+          <h1>QTech Dash</h1>
+          <p>Tu Agregador de Juegos Favorito</p>
+      </header>
+      <main className="app">
+        <section className="card">
+          <h1>QTech Dash</h1>
+          <p>React is now configured with Vite.</p>
+        </section>
+      </main>
+    </>
   );
 }
 
